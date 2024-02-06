bba85194aabb74397f0f7884556ca4dc26550431
Matsatsi14 <153347954+Matsatsi14@users.noreply.github.com>
2024-02-06 16:24:34 +0200

    Added a footer. A Github profile URL, a Github repository URL and Netlify URL

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        Coded by <a href="https://github.com/Matsatsi14/">Matsatsi Mashatola</a>
+        , open-sourced on{" "}
+        <a href="https://github.com/Matsatsi14/react-weather-app">GitHub</a> and
+        hosted on{" "}
+        <a href="https://react-weather-tsatsi.netlify.app/">Netlify</a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
