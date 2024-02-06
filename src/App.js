1fb385c2ce9f29359068bb0335686eb7a70870ee
Matsatsi14 <153347954+Matsatsi14@users.noreply.github.com>
2024-02-06 16:47:12 +0200

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
+import React from "react";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        Coded by <a href="https://github.com/Matsatsi14/">Matsatsi Mashatola</a>
-        , open-sourced on{" "}
-        <a href="https://github.com/Matsatsi14/react-weather-app">GitHub</a> and
-        hosted on{" "}
-        <a href="https://react-weather-tsatsi.netlify.app/">Netlify</a>
-      </footer>
+      <div className="container">
+        Weather App
+        <footer>
+          Coded by{" "}
+          <a href="https://github.com/Matsatsi14/">Matsatsi Mashatola</a>,
+          open-sourced on{" "}
+          <a href="https://github.com/Matsatsi14/react-weather-app">GitHub</a>{" "}
+          and hosted on{" "}
+          <a href="https://react-weather-tsatsi.netlify.app/">Netlify</a>
+        </footer>
+      </div>
     </div>
   );
 }
